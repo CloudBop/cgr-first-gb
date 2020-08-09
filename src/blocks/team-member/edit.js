f1bae3e9ef478a4b51733da5d576e0bb9ae04536
Cloudbop <boppincolin@gmail.com>
2020-08-09 18:24:47 +0100

    fixed onUploadError handler, implemented toolbar in edit

--- src/blocks/team-member/edit.js
+++ src/blocks/team-member/edit.js
@@ -1,8 +1,19 @@
 import { Component } from "@wordpress/element";
-import { RichText, MediaPlaceholder } from "@wordpress/editor";
+import {
+    RichText,
+    MediaPlaceholder,
+    BlockControls,
+    MediaUploadCheck,
+    MediaUpload
+} from "@wordpress/editor";
 import { __ } from "@wordpress/i18n";
 import { isBlobURL } from "@wordpress/blob";
-import { Spinner, withNotices } from "@wordpress/components";
+import {
+    Spinner,
+    withNotices,
+    Toolbar,
+    IconButton
+} from "@wordpress/components";
 class TeamMemberEdit extends Component {
     constructor(props) {
         super(props);
@@ -30,50 +41,107 @@ class TeamMemberEdit extends Component {
     onSelectUrl = ({ url }) =>
         this.props.setAttributes({ url, id: null, alt: "" });
     //
-    onUploadError = msg => console.log(msg);
+    onUploadError = msg => {
+        const noticeOperations = this.props;
+        noticeOperations.createErrorNotice(msg);
+    };
+
+    onRemoveImage = () =>
+        this.props.setAttributes({ url: "", id: null, alt: "" });
+    //
     render() {
         // wp generated classname
         const { className, attributes, noticeUI } = this.props;
-        const { title, info, url, alt } = attributes;
+        const { title, info, url, alt, id } = attributes;
         return (
-            <div className={className}>
-                {url ? (
-                    <>
-                        <img src={url} alt={alt} />
-                        {isBlobURL(url) && <Spinner />}
-                    </>
-                ) : (
-                    //
-                    <MediaPlaceholder
-                        icon={"format-image"}
-                        onSelect={this.onSelectImage}
-                        onSelectURL={this.onSelectUrl}
-                        onError={this.onUploadError}
-                        accept={"image/*"}
-                        allowedTypes={["image"]}
-                        // - errors
-                        notices={noticeUI}
+            <>
+                <BlockControls>
+                    {url && (
+                        <Toolbar>
+                            <IconButton
+                                // to generate default WP hover effect
+                                className="components-icon-button
+                            components-toolbar__control"
+                                //
+                                label={__("Remove Image", "cgr-first-gb")}
+                                onClick={this.onRemoveImage}
+                                icon="trash"
+                            />
+                            {/* check user is authorized to edit/upload image */}
+
+                            {
+                                // only display edit if image has been uploaded to media library, not if set as external url
+                                id && (
+                                    <MediaUploadCheck>
+                                        <MediaUpload
+                                            // current id
+                                            value={id}
+                                            allowedTypes={["image"]}
+                                            onSelect={this.onSelectImage}
+                                            // destructure open media lib provided by render fn
+                                            render={({ open }) => {
+                                                return (
+                                                    <IconButton
+                                                        className="components-icon-button
+                                            components-toolbar__control"
+                                                        //
+                                                        label={__(
+                                                            "Edit Image",
+                                                            "cgr-first-gb"
+                                                        )}
+                                                        onClick={open}
+                                                        icon="edit"
+                                                    />
+                                                );
+                                            }}
+                                        />
+                                    </MediaUploadCheck>
+                                )
+                            }
+                        </Toolbar>
+                    )}
+                </BlockControls>
+                <div className={className}>
+                    {url ? (
+                        <>
+                            <img src={url} alt={alt} />
+                            {isBlobURL(url) && <Spinner />}
+                        </>
+                    ) : (
+                        //
+                        <MediaPlaceholder
+                            icon={"format-image"}
+                            onSelect={this.onSelectImage}
+                            onSelectURL={this.onSelectUrl}
+                            onError={this.onUploadError}
+                            accept={"image/*"}
+                            allowedTypes={["image"]}
+                            // - errors
+                            notices={noticeUI}
+                        />
+                    )}
+                    <RichText
+                        className={
+                            "wp-block-cgr-first-gb-blocks-team-member__title"
+                        }
+                        tagName={"h4"}
+                        onChange={this.onChangeTitle}
+                        value={title}
+                        placeholder={__("Member Name", "cgr-first-gb")}
+                        formattingControls={[]}
                     />
-                )}
-                <RichText
-                    className={
-                        "wp-block-cgr-first-gb-blocks-team-member__title"
-                    }
-                    tagName={"h4"}
-                    onChange={this.onChangeTitle}
-                    value={title}
-                    placeholder={__("Member Name", "cgr-first-gb")}
-                    formattingControls={[]}
-                />
-                <RichText
-                    className={"wp-block-cgr-first-gb-blocks-team-member__info"}
-                    tagName={"p"}
-                    onChange={this.onChangeInfo}
-                    value={info}
-                    placeholder={__("Member Info", "cgr-first-gb")}
-                    formattingControls={[]}
-                />
-            </div>
+                    <RichText
+                        className={
+                            "wp-block-cgr-first-gb-blocks-team-member__info"
+                        }
+                        tagName={"p"}
+                        onChange={this.onChangeInfo}
+                        value={info}
+                        placeholder={__("Member Info", "cgr-first-gb")}
+                        formattingControls={[]}
+                    />
+                </div>
+            </>
         );
     }
 }
