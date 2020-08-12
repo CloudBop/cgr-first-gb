2b57452f7fd5523f6169736c728b0e17d8ce354b
Cloudbop <boppincolin@gmail.com>
2020-08-13 00:46:30 +0100

    added InspectorControls for block, updated withSelect call to DB for admin GB error

--- src/blocks/latest-posts/edit.js
+++ src/blocks/latest-posts/edit.js
@@ -3,15 +3,61 @@ import { Component } from "@wordpress/element";
 import { withSelect } from "@wordpress/data";
 import { __ } from "@wordpress/i18n";
 import { decodeEntities } from "@wordpress/html-entities";
+import { PanelBody, RangeControl, SelectControl } from "@wordpress/components";
+import { InspectorControls } from "@wordpress/editor";
 //
 class LatestPostsEdit extends Component {
     //
+
+    onChangeNumberOfPosts = numberOfPosts =>
+        this.props.setAttributes({ numberOfPosts });
+    onChangePostListCategories = selectedCategories =>
+        this.props.setAttributes(
+            // stringify - this set in php as is dynamic block
+            { postCategories: selectedCategories.join(",") }
+        );
+
     render() {
         // wp generated stuff
-        const { posts, className } = this.props;
+        const { posts, className, attributes, categories } = this.props;
+        // set in php
+        const { numberOfPosts, postCategories } = attributes;
         //
+        console.log("this.props", this.props);
         return (
             <>
+                {
+                    <InspectorControls>
+                        <PanelBody title={__("Posts Settings", "cgr-first-gb")}>
+                            <RangeControl
+                                label={__("Number of posts", "cgr-first-gb")}
+                                value={numberOfPosts}
+                                onChange={this.onChangeNumberOfPosts}
+                                min={1}
+                                max={10}
+                                step={1}
+                            />
+
+                            <SelectControl
+                                // make array
+                                multiple
+                                label={__("Categories", "cgr-first-gb")}
+                                onChange={this.onChangePostListCategories}
+                                options={
+                                    categories &&
+                                    categories.map(category => ({
+                                        value: category.id,
+                                        label: category.name
+                                    }))
+                                }
+                                // comma seperated string to array
+                                value={
+                                    postCategories && postCategories.split(",")
+                                }
+                            />
+                        </PanelBody>
+                    </InspectorControls>
+                }
                 {posts && posts.length > 0 ? (
                     <ul className={className}>
                         {posts.map((post, idx) => (
@@ -41,10 +87,27 @@ class LatestPostsEdit extends Component {
 
 export default withSelect((select, props) => {
     const { attributes } = props;
-    const { numberOfPosts } = attributes;
-    let query = { per_page: numberOfPosts };
+    const { numberOfPosts, postCategories } = attributes;
+
+    let numberOfPagesQuery = { per_page: numberOfPosts };
+    if (postCategories) {
+        // only get selected categories
+        numberOfPagesQuery["categories"] = postCategories;
+    }
+    // -1 === get  all the categories
+    let allCategoriesQuery = { per_page: -1 };
 
     return {
-        posts: select("core").getEntityRecords("postType", "post", query)
+        // using the rest API
+        posts: select("core").getEntityRecords(
+            "postType",
+            "post",
+            numberOfPagesQuery
+        ),
+        categories: select("core").getEntityRecords(
+            "taxonomy",
+            "category",
+            allCategoriesQuery
+        )
     };
 })(LatestPostsEdit);
