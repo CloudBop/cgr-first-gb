01a6a265d0cca1a2a4a0622acfa0c257654b955b
Cloudbop <boppincolin@gmail.com>
2020-08-12 15:00:57 +0100

    implemented withSelect to connect to WP redux.

--- src/blocks/latest-posts/edit.js
+++ src/blocks/latest-posts/edit.js
@@ -1,9 +1,19 @@
 import { Component } from "@wordpress/element";
+import { withSelect } from "@wordpress/data";
 
 class LatestPostsEdit extends Component {
     render() {
+        console.log(this.props);
         return <p>test</p>;
     }
 }
 
-export default LatestPostsEdit;
+export default withSelect((select, props) => {
+    const { attributes } = props;
+    const { numberOfPosts } = attributes;
+    let query = { per_page: numberOfPosts };
+
+    return {
+        posts: select("core").getEntityRecords("postType", "post", query)
+    };
+})(LatestPostsEdit);
